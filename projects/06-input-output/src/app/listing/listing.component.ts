a96ace7cd5317145eea29f6eba7537c375399242
Lívia <liviapnd@gmail.com>
2026-02-21 19:39:46 -0300

    feat(hands-on): inputs, outputs and events

--- projects/06-input-output/src/app/listing/listing.component.ts
+++ projects/06-input-output/src/app/listing/listing.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Car } from '../car';
 
@@ -6,7 +6,46 @@ import { Car } from '../car';
   selector: 'app-listing',
   standalone: true,
   imports: [CommonModule],
-  template: ` <!-- listing markup goes here --> `,
+  template: ` <!-- This article element represents and entire listing -->
+    @for (car of carList; track car) {
+      <article class="listing">
+        <div class="image-parent">
+          <img class="product-image" src="https://placehold.co/100x100" />
+        </div>
+        <section class="details">
+          <p class="title">{{car.make}} {{car.model}}</p>
+          <hr />
+          <p class="detail">
+            <span>Year</span>
+            <span>{{ car.year }}</span>
+          </p>
+          <div class="detail">
+            <span>Transmission</span>
+            <span>{{ car.transmission }}</span>
+          </div>
+          <p class="detail">
+            <span>Mileage</span>
+            <span>{{ car.miles }}</span>
+          </p>
+          <p class="detail">
+            <span>Price</span>
+            <span>&dollar;{{ car.price }}</span>
+          </p>
+        </section>
+        <button (click)="saveCar(car)">Save</button>
+      </article>
+    } @empty {
+      <p>No cars available.</p>
+    }
+      <!-- end car listing markup -->`,
   styles: ``,
 })
-export class ListingComponent {}
+export class ListingComponent {
+  @Input() carList?: Car[];
+
+  @Output() handleCarSaved = new EventEmitter<Car>();
+
+  saveCar(car: Car) {
+    this.handleCarSaved.emit(car);
+  }
+}
