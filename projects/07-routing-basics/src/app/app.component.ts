b4e52a3ec737270996044be95351c1bf7a7c421f
Livia Nascimento <liviaraianen@gmail.com>
2026-03-15 00:38:23 -0300

    feat(routing) componente greetings adicionado ao default path

--- projects/07-routing-basics/src/app/app.component.ts
+++ projects/07-routing-basics/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
-  template: ` <h1>Enable routing to see the greeting below</h1> `,
+  template: ` <router-outlet /> `,
   styles: [],
+  imports: [RouterModule],
 })
 export class AppComponent {
   title = '07-routing-basics';
